8a5751199b0bc5c05bb5559c7c413daa31dd9acc
rathaiuit <ratha.baby007@gmail.com>
2015-04-01 05:39:55 -0700

    hello

---
+++ Include/main.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h1>Welcome to my home page!</h1>
+<p>Some text.</p>
+<p>Some more text.</p>
+<?php include("left.php");?>
+<?php include("footer.php");?>
+<?php include("header.php");?>
+<?php include("right.php");?>
+
+</body>
+</html> 
